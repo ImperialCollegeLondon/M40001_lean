fdb4388a9765f2c6ce9e71209403ba3d0113d80c
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2019-09-17 19:23:44 +0100

    adding Lean repo

---
+++ src/questions/sheet1.lean
@@ -0,0 +1,18 @@
+/-  Math40001 : Introduction to university mathematics.
+
+Problem Sheet 1, October 2019.
+
+Translated from pdf into Lean by Kevin Buzzard 
+-/
+
+-- Throughout these questions, let P, Q, R, S be true-false statements.
+variables {P Q R S: Prop}
+
+/-
+Question 1. Prove that P ∧ Q implies Q ∧ P. 
+-/
+lemma question_one : P ∧ Q → Q ∧ P :=
+begin
+  sorry
+end
+
