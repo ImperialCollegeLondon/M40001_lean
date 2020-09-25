021c14779ebb716be3454f083d716f1df5595d2a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-09-25 16:19:37 +0100

    take 2

--- src/2020/sets/lean_lecture.lean
+++ src/2020/sets/lean_lecture.lean
@@ -1,8 +1,8 @@
-import tactic 
+import tactic
 
 import data.real.basic
 
-example : ∀ x : ℝ, ∃ y : ℝ, x + y ≥ 0 :=
+example : ∀ x : ℝ, ∃ y : ℝ, x + y > 0 :=
 begin
   intro x,
   use 37 - x,
@@ -10,38 +10,38 @@ begin
   norm_num,
 end
 
--- example : ∃ y : ℝ, ∀ x : ℝ, x + y ≥ 0 :=
+-- example : ∃ y : ℝ, ∀ x : ℝ, x + y > 0 :=
 -- begin
---   use 100000000000000000000,
+--   use 10000000000,
 --   intro x,
---   -- this is impossible
+--   -- stuck!
 --   sorry
 -- end
 
-example : ¬ (∃ y : ℝ, ∀ x : ℝ, x + y ≥ 0) :=
+example : ¬ (∃ y : ℝ, ∀ x : ℝ, x + y > 0) :=
 begin
   push_neg,
   intro y,
-  use -37-y,
+  use -37 - y,
   simp,
   norm_num,
 end
 
--- construction 1 and construction 2
--- showed that predicates on α corresponded to subsets of α
-
 variable (α : Type)
 
-example : (α → Prop) ≃ set α :=
+example : (α → Prop) ≃ set α := 
 { to_fun := λ P, {x : α | P x},
   inv_fun := λ X, λ a, a ∈ X,
   left_inv := begin
     intro P,
     dsimp,
     refl,
-  end,
+  end
+  ,
   right_inv := begin
     intro X,
     dsimp,
     refl,
   end }
+
+
