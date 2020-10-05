b6ff309e1c24fb5f52f5f5efdd30c80bc6be42d3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-10-05 14:41:16 +0100

    final version

--- src/2020/functions/injective_comp.lean
+++ src/2020/functions/injective_comp.lean
@@ -6,36 +6,15 @@ variables (X Y Z : Type) (f : X → Y) (g : Y → Z)
 
 example : (injective f) ∧ (injective g) → injective (g ∘ f) :=
 begin
-  intro hfg,
-  cases hfg with hf hg,
-  unfold injective at *,
-  intros a b,
-  intro hab,
+  -- Assume that f and g are injective.
+  rintro ⟨hf, hg⟩,
+  -- We need to prove `g ∘ f` is injective.
+  -- So say `a`, `b` are in `X`, and `(g ∘ f)(a) = (g ∘ f)(b)` 
+  rintro a b hab,
+  -- We need to prove that `a = b`.
+  -- By injectivity of `f`, it suffices to prove that `f(a) = f(b)`.
   apply hf,
-  apply hg,
-  exact hab,
-end
-
-example : (surjective f) ∧ (surjective g) → surjective (g ∘ f) :=
-begin
-      -- Maths proof
-
-      -- Say f and g are surjective.
-  rintros ⟨hf, hg⟩,
-      -- Choose  z in Z.
-  intro z,
-      -- Our job
-      -- is to find x in X such that g(f(x))=z.
-
-      -- Well g is assumed surjective so we can find y in Y
-      -- such that g(y)=z
-  rcases hg z with ⟨y, rfl⟩,
-
-      -- And f is assumed surjective so we can find x in X
-      -- such that f(x)=y
-  rcases hf y with ⟨x, rfl⟩,
-
-   -- let's use this x.
-  use x,
-      -- and now we see g(f(x))=g(y)=z so we're done
+  -- But this follows immediately from our assumption `g(f(a))=g(f(b))`,
+  -- and injectivity.
+  exact hg hab,
 end
