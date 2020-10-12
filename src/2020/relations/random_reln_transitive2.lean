cbf65186b4b2a7e3451000e991eec196c2041226
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-10-12 14:59:49 +0100

    transitivity of random relation

--- src/2020/relations/random_reln_transitive2.lean
+++ src/2020/relations/random_reln_transitive2.lean
@@ -9,20 +9,36 @@ inductive X
 open X
 
 def R (x y : X) : Prop := (x = A ∧ y = B) ∨ (x = A ∧ y = C)
+-- R(A,B) and R(A,C) both true, everything else false
 
 theorem R_fst {x y : X} : R x y → x = A :=
 begin
-  sorry
+  intro h,
+  cases h,
+    cases h,
+    assumption,
+  cases h,
+  assumption,
 end
 
 example : transitive R :=
 begin
-  sorry
+  intros x y z,
+  intro hxy,
+  intro hyz,
+  have h1 := R_fst hxy,
+  have h2 := R_fst hyz,
+  subst h1,
+  subst h2,
+  assumption,
 end
 
 example (P Q : Prop) : (P → Q) → (¬ Q → ¬ P) :=
 begin
-  sorry
+  intro hPQ,
+  intro hnQ,
+  intro hP,
+  apply hnQ,
+  exact hPQ hP,
 end
-
 
