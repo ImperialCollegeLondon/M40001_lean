9bc2eabb7f9c7402bdbf87036d0e4b90212eb25b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-10-12 15:00:09 +0100

    relations video

--- src/2020/relations/equality_nonsense.lean
+++ src/2020/relations/equality_nonsense.lean
@@ -5,18 +5,23 @@ variables (α : Type) (x y z : α)
 --set_option pp.notation false
 example : x = x :=
 begin
-  sorry
+  refl,
 end
 
 
 example : x = y → y = x :=
 begin
-  sorry
+  intro h,
+  induction h,
+  refl,
 end
 
 example : x = y → y = z → x = z :=
 begin
-  sorry
+  intro hxy,
+  intro hyz,
+  induction hxy,
+  assumption,
 end
 
 
