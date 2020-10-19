6bb8575e793a1c4dec8b56cc2bd2503090a2e654
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-10-19 11:38:35 +0100

    part 2 sheet 1 half of

---
+++ src/2020/problem_sheets/Part_II_sheet1.lean
@@ -0,0 +1,59 @@
+import tactic
+
+variables (x y : ℕ)
+
+theorem Q1a : x + y = y + x :=
+begin
+  sorry
+end
+
+theorem Q1b : x + y = x → x = 0 :=
+begin
+  sorry
+end
+
+theorem Q1c : x + y = 0 → x = 0 ∧ y = 0 :=
+begin
+  sorry
+end
+
+theorem Q1d : x * y = y * x :=
+begin
+  sorry
+end
+
+theorem Q2a : 1 * x = x ∧ x = x * 1 :=
+begin
+  sorry
+end
+
+variable z : ℕ
+
+theorem Q2b : (x + y) * z = x * z + y * z :=
+begin
+  sorry
+end
+
+theorem Q2c : (x * y) * z = x * (y * z) :=
+begin
+  sorry
+end
+
+
+-- Q3 def
+def is_pred (x y : ℕ) := x.succ = y
+
+theorem Q3a : ¬ ∃ x : ℕ, is_pred x 0 :=
+begin
+  sorry
+end
+
+theorem Q3b : y ≠ 0 → ∃! x, is_pred x y :=
+begin
+  sorry
+end
+
+
+
+
+
