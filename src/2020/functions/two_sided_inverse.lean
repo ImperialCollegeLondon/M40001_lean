a6391e337d05af5d36e19c368cde283cf18923c7
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-10-22 18:08:59 +0100

    two sided inverse reads better than tsi

--- src/2020/functions/two_sided_inverse.lean
+++ src/2020/functions/two_sided_inverse.lean
@@ -10,44 +10,43 @@ is a bijection if and only if it has a two-sided inverse.
 variables {X  Y : Type} (f : X → Y)
 
 -- two-sided inverse
-
-
-structure tsi (f : X → Y) :=
+structure two_sided_inverse (f : X → Y) :=
 (g : Y → X)
 (hX : ∀ x : X, g (f x) = x)
 (hY : ∀ y : Y, f (g y) = y)
 
 open function
 
-example : bijective f ↔ nonempty (tsi f) :=
+example : bijective f ↔ nonempty (two_sided_inverse f) :=
 begin
   split,
   { intro hf,
     cases hf with hi hs,
     choose g hg using hs,
-    let G : tsi f := 
-    {  g := g,
+    let G : two_sided_inverse f :=
+    { g := g,
       hX := begin
         intro x,
         apply hi,
-        rw hg
+        rw hg,
       end,
       hY := begin
         exact hg,
-      end },
-    use G
+      end
+    },
+    use G,
   },
   { rintro ⟨g, hX, hY⟩,
     split,
     { intros a b hab,
       apply_fun g at hab,
-      rw [hX, hX] at hab,
+      rw hX at hab,
+      rw hX at hab,
       assumption
     },
     { intro y,
-      use g y,
+      use (g(y)),
       apply hY,
     }
   }
 end
-
