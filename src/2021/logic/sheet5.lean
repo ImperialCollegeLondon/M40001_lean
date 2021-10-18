97e62256219e8b5f5de5c7bd55a6d5620b9535a6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-10-18 13:50:25 +0100

    add tactic descns

--- src/2021/logic/sheet5.lean
+++ src/2021/logic/sheet5.lean
@@ -12,19 +12,24 @@ import tactic -- imports all the Lean tactics
 
 We learn about how to manipulate `P ↔ Q` in Lean.
 
+## Tactics
+
 You'll need to know about the tactics from the previous sheets,
 and also the following two new tactics:
 
 * `refl`
 * `rw`
 
-Rules of thumb: 
+## The `refl` tactic
 
-If your goal is `P ↔ P` then `refl` will solve it.
+If your goal is `P ↔ P` then `refl,` will solve it.
 
-If `hPiQ : P ↔ Q` is a hypothesis, you can decompose it
-using `cases hPiQ with hPQ hQP`. However, if you keep
-it around then you can do `rw hPiQ` which changes all `P`s to `Q`s.
+## The `rw` tactic
+
+If `h : P ↔ Q` is a hypothesis, you can decompose it
+using `cases h with hPQ hQP,`. However, if you keep
+it around then you can do `rw h,` which changes all `P`s in the goal to `Q`s.
+Variant: `rw h at h2,` will change all `P`s to `Q`s in hypothesis `h2`.
 
 -/
 
