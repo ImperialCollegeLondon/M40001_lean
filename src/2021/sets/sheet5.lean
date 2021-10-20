b6601b99cb9c871a33683a6a2589221e410bd50d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-10-20 17:09:55 +0100

    rintro and rcases

--- src/2021/sets/sheet5.lean
+++ src/2021/sets/sheet5.lean
@@ -28,6 +28,50 @@ For example if your goal is `⊢ x ∈ A ∩ B` then you could write
 also use the `split,` tactic directly, and this will immediately turn the goal
 into two goals `⊢ x ∈ A` and `⊢ x ∈ B`.
 
+## New tactics you will need
+
+You don't need to know any new tactics to solve this sheet. I've mentioned
+the `change` tactic. You don't have to use it, and if you use it your
+proofs will get longer. So in return I'll tell you about two other
+tactics, `rcases` and `rintro`, which you don't have to use either
+but if you use them they'll make your proofs shorter.
+
+### The `rcases` tactic
+
+`rcases` is a souped-up version of `cases`. It has slightly different
+syntax. If you have a hypothesis `h : P ∧ Q` then `cases h with hP hQ`
+and `rcases h with ⟨hP, hQ⟩` do the same thing. However, if you
+have a hypothesis `h : P ∧ Q ∧ R` then Lean interprets it as `P ∧ (Q ∧ R)`
+so if you want to destruct it with `cases` you have to do
+
+```
+cases h with hP hQR,
+cases hQR with hQ hR
+```
+
+You can do this all in one go with `rcases h with ⟨hP, hQ, hR⟩`. The
+name `rcases` stands for "recursive cases".
+
+### The `rintro` tactic
+
+It's quite common to find yourself doing `intro` then `cases` or,
+more generally, `intro` then `rcases`. The `rintro` tactic does
+these both at once! So for example if your goal is
+
+```
+⊢ (P ∧ Q) → R
+```
+
+then `rintro ⟨hP, hQ⟩` leaves you at
+
+```
+hP : P
+hQ : Q
+⊢ R
+```
+
+i.e. the same as `intro h, cases h with hP hQ,`
+
 -/
 
 open set
